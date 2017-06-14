0d3c27612d021835be68778a8b16d3519c494f24
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-13 22:44:03 -0400

    add autocomplete components

--- src/SearchComponent/SearchInput.js
+++ src/SearchComponent/SearchInput.js
@@ -13,7 +13,7 @@ class SearchInput extends Component {
     render(){
         return(
             <div className="search-input">
-                <input type="text"/>
+                <input type="text" placeholder="Enter movie here..."/>
                 <button>Search</button>
             </div>
         )

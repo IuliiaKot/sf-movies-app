e9e6c206504d1fbabc1d8eaf79c5cf7baaf17b83
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-14 10:27:38 -0400

    add movieInfo component and style

---
+++ src/MovieList/MovieInfo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './MovieInfo.css'
+
+
+class MovieInfo extends Component {
+    render() {
+        return (
+            <div className='info'>
+                heloo
+            </div>
+        )
+    }
+}
+
+
+export default MovieInfo;
--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import './MovieList.css'
+import MovieInfo from './MovieInfo'
 
 
 class MovieList extends Component {
     render() {
         return(
             <div className="movie-list">
-               <div className='info'>
-                   heloo
-               </div>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               <MovieInfo/>
+               
             </div>  
         )
     }

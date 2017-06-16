7dcd73c1e753e12e3d09c182cb072fc1e5e74515
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-16 14:29:33 -0400

    add fun_fuct for movie

--- src/MovieList/MovieInfo.js
+++ src/MovieList/MovieInfo.js
@@ -4,13 +4,14 @@ import './MovieInfo.css'
 
 class MovieInfo extends Component {
     render() {
-        let {title, locations, release_year} = {...this.props.info}
+        let {title, locations, release_year, fun_facts} = {...this.props.info}
         return (
             <div className='info'>
                 <ul className="">
                     <li>{title}</li>
                     <li>{locations}</li>
                     <li>{release_year}</li>
+                    <li>{fun_facts}</li>
                 </ul>
             </div>
         )

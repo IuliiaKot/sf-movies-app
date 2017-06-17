70a0d93be75b9127f68c0b5baf83e7528284fe7d
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-17 09:27:11 -0400

    display actors

--- src/MovieList/MovieInfo.js
+++ src/MovieList/MovieInfo.js
@@ -5,13 +5,15 @@ import './MovieInfo.css'
 class MovieInfo extends Component {
     render() {
         let {title, locations, release_year, fun_facts, actor_1, actor_2, actor_3} = {...this.props.info}
+        let actor = (actor_1 && actor_2) ? <p><strong>Actors: </strong>{actor_1}, {actor_2}</p> : null 
         return (
             <div className='info'>
                 <div className="card">
                     <h3>{title}</h3>
-                    <p><strong>Location: </strong> {locations}</p>
+                    {locations ? <p><strong>Location: </strong> {locations}</p> : null}
                     <p><strong>Release Year: </strong> {release_year}</p>
                     {fun_facts ? <p><strong>Fun Fucts: </strong> {fun_facts}</p> : null}
+                    {actor}
                 </div>
             </div>
         )

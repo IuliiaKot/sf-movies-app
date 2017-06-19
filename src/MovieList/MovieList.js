c9fc01fb3e295b6e262c0b2ee5944b98a39f732e
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-18 23:46:29 -0400

    add style for mobile

--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -4,16 +4,35 @@ import MovieInfo from './MovieInfo'
 
 
 class MovieList extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            hide:  false
+        }
+        this.hideList = this.hideList.bind(this);
+    }
+    hideList() {
+        this.setState((prevState) => {
+            return {hide: !prevState.hide}
+        })
+    }
+
     render() {
-        return(
+        let display = (!this.state.hide) ? 
             
-            <div className="movie-list">
+            <div className="movie-list" id={this.state.hide ? 'hideList': ''} >
+                <span onClick={this.hideList}>Hide</span>
                {
                    this.props.moviesList.map((movie,id) => {
                        return <MovieInfo key={id} info={movie}/>
                    })
                }
             </div>  
+            :
+            <div className="movie-list" > 
+                <span onClick={this.hideList}>Show</span></div>
+        return(
+            <div>{ display}</div>            
         )
     }
 }
